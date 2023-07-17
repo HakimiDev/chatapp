103611b4bb5fa22a50f630346375118aab14882c
HakimiDev <hakimideveloper@gmail.com>
2023-07-18 00:57:03 +0300

    new

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="min-h-screen min-w-full flex flex-col justify-center items-center">
-        <h2 class="text-6xl">404</h2>
-        <p class="text-3xl">This page is not found!</p>
-        <div class="min-w-full flex justify-center items-center">
-            <a href="#"><Button class="w-full mt-4">Back Home</Button></a>
+        <h2 class="text-6xl font-extrabold">4<span class="text-gray-400">0</span>4</h2>
+        <p class="text-3xl font-semibold text-center">This page is not found!</p>
+        <div class="min-w-full flex justify-center items-center mt-4">
+            <a href="#" class="w-[50vw]"><Button class="w-full">Back Home</Button></a>
         </div>
     </div>
 </template>
