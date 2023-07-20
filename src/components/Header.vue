ad572cee3b56e43f8bd739210d689a4ea661bed5
HakimiDev <115172576+HakimiDev@users.noreply.github.com>
2023-07-20 06:12:30 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,8 @@
             <div class="text-xl max-xs:text-lg font-bold">
                 <h2>Chat<span class=" text-orange-300">App</span></h2>
             </div>
-            <div @click="toogleShowSettings" class="w-fit cursor-pointer transition duration-500 hover:-rotate-180">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <div class="w-fit cursor-pointer transition duration-500 hover:-rotate-180">
+                <svg @click="toogleShowSettings" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
