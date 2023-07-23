cca09fd299353c9b77100d9bd120b7dfcd5ce2f2
HakimiDev <hakimideveloper@gmail.com>
2023-07-23 15:17:45 +0300

    new

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,18 +1,18 @@
 <template>
     <li :class="message.byMe ? 'justify-end' : 'justify-start'" class="min-w-full h-fit flex p-1">
-        <div :class="[message.byMe ? ' bg-secondary-200' : 'bg-primary-300', (messages[index - 1] && message.byMe === messages[index - 1].byMe) ? 'ms-1 me-1' : '']"
-            class="relative p-2 inline-block max-w-[65%] h-fit break-words rounded-lg">
+        <div :class="[message.byMe ? ' bg-secondary-200' : 'bg-primary-300', (messages[index - 1] && message.byMe === messages[index - 1].byMe) ? 'ms-1 me-1' : (message.byMe) ? 'rounded-tr-none' : 'rounded-tl-none']"
+            class="relative py-2 px-4 inline-block max-w-[65%] h-fit break-words rounded-3xl">
             <div
                 v-if="message.byMe && (!messages[index - 1] || message.byMe !== messages[index - 1].byMe)"
-                class="absolute top-[4px] mt-[-6px]  right-[-10px] ml-[-5px] border-l-[12px] border-r-[12px] border-transparent border-b-[12px] border-b-secondary-200 -rotate-[42deg]">
+                class="absolute top-[4px] mt-[-6px]  right-[-10px] ml-[-5px] border-l-[12px] border-r-[12px] border-transparent border-b-[12px] border-b-secondary-200 -rotate-[45deg] rounded-lg">
             </div>
             <div
                 v-else-if="!message.byMe && (!messages[index - 1] || message.byMe !== messages[index - 1].byMe)"
-                class="absolute top-[4px] mt-[-6px]  left-[-10px] mr-[-5px] border-r-[12px] border-l-[12px] border-transparent border-b-[12px] border-b-primary-300 rotate-[42deg]">
+                class="absolute top-[4px] mt-[-6px]  left-[-10px] mr-[-5px] border-r-[12px] border-l-[12px] border-transparent border-b-[12px] border-b-primary-300 rotate-[45deg] rounded-lg">
             </div>
 
             <p v-html="message.content"></p>
-            <div class="flex flex-row justify-end items-center text-gray-400">
+            <div class="flex flex-row justify-end items-center text-gray-400 text-sm">
                 <p>{{ message.time }}</p>
                 <span class="flex flex-row text-blue-500" v-if="message.byMe">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.3"
--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -2,7 +2,7 @@
     <div class="relative h-fit">
         <img class="w-14 h-14 max-xs:w-12 max-xs:h-12 border-2 border-gray-400 rounded-full object-cover" :src="user.avatarUrl"
             :alt="index">
-        <div :class="user.isOnline ? 'bg-secondary-400' : 'bg-gray-500'"
+        <div :class="user.isOnline ? 'bg-green-500' : 'bg-gray-500'"
             class="h-4 w-4 absolute bottom-0 right-0 mb-1 rounded-full"></div>
     </div>
 </template>
