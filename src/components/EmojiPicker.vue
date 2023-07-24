8e510f0dc4caa416cc17738a7e0f1fcb6610cb5e
HakimiDev <hakimideveloper@gmail.com>
2023-07-24 17:03:45 +0300

    new

--- src/components/EmojiPicker.vue
+++ src/components/EmojiPicker.vue
@@ -1,7 +1,7 @@
 <template>
     <Transition>
         <div v-show="show" class="min-w-full h-80">
-            <div class="bg-primary-300 min-w-full h-full overflow-y-auto overflow-x-hidden">
+            <div class="bg-primary-300 min-w-full h-full overflow-y-auto overflow-x-hidden relative">
                 <header class="min-w-full flex border-b-[1px] border-gray-400">
                     <ul class="min-w-full grid grid-cols-8 max-xs:grid-cols-4">
                         <li v-for="(category, index) in Object.keys(emojis)" :key="index" @click="selectCategory(category)"
@@ -22,14 +22,16 @@
                             </li>
                         </ul>
                     </div>
-                    <div v-else class="min-w-full h-full text-center p-10">
+                    <div v-else class="min-w-full min-h-full text-center p-20">
                         <p class="text-3xl">No emojis!</p>
                     </div>
                 </main>
 
-                <footer class=" sticky bottom-0 min-w-full bg-primary-100 p-2">
-                    <div class="min-w-full flex flex-row justify-end items-center bg-primary-100">
-                        <div @click="remove"
+
+            </div>
+            <footer class=" sticky bottom-0 min-w-full bg-primary-100 p-2">
+                <div class="min-w-full flex flex-row justify-end items-center bg-primary-100">
+                    <div @click="remove"
                         class="cursor-pointer rounded-full p-2 transition duration-500 hover:bg-secondary-50">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
@@ -37,9 +39,8 @@
                                 d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
                         </svg>
                     </div>
-                    </div>
-                </footer>
-            </div>
+                </div>
+            </footer>
         </div>
     </Transition>
 </template>
