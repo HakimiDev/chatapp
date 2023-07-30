6b05e2527c1cea1bf66b0b8923be317b9fd23284
HakimiDev <hakimideveloper@gmail.com>
2023-07-30 20:56:42 +0300

    some edits

--- src/components/EmojiPicker.vue
+++ src/components/EmojiPicker.vue
@@ -54,8 +54,8 @@ const props = defineProps(['show', 'onAppend', 'onRemove']);
 const appendEmoji = (emoji) => {
     props.onAppend(emoji.native);
     if (lastUsedEmojis.value.length >= 20) lastUsedEmojis.value.pop();
-    // if (lastUsedEmojis.value.find(e => e.native === emoji.native)) lastUsedEmojis.value = lastUsedEmojis.value.filter(e => e.native !== emoji.native);
-    lastUsedEmojis.value.unshift(emoji);
+    if (!lastUsedEmojis.value.find(e => e.native === emoji.native)) lastUsedEmojis.value.unshift(emoji);
+    //lastUsedEmojis.value = lastUsedEmojis.value.filter(e => e.native !== emoji.native);
     onLastUsedEmojisChange();
 };
 
